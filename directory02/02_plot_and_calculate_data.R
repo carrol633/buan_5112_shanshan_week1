7d23dd6cf1b017f3e8eb68382ee760a626fa4e7e
”Shanshan <sliu3@seattleu.edu>
2022-10-05 00:47:28 -0700

    Update data file directory

--- directory02/02_plot_and_calculate_data.R
+++ directory02/02_plot_and_calculate_data.R
@@ -2,7 +2,7 @@ library(tidyverse)
 library(haven)
 
 # load data and save into new data frame
-nfhs <- read_dta("IAHR52FL.dta")
+nfhs <- read_dta("data_directory/IAHR52FL.dta")
 
 # select the variables and save into new data frame
 hh <- select(nfhs, hhid:shstruc) 
